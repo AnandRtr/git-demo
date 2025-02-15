cbe925c687e4f9747d3824364f3ee47b538deafa
AnandRtr <ananthananth2315@gmail.com>
2025-02-15 17:18:00 +0530

    new

---
+++ src/main/java/com/example/git/demo/Product.java
@@ -0,0 +1,31 @@
+package com.example.git.demo;
+
+public class Product {
+    private int ProductId;
+    private String ProductName;
+    private int ProductInt;
+
+    public int getProductId() {
+        return ProductId;
+    }
+
+    public void setProductId(int productId) {
+        ProductId = productId;
+    }
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public void setProductName(String productName) {
+        ProductName = productName;
+    }
+
+    public int getProductInt() {
+        return ProductInt;
+    }
+
+    public void setProductInt(int productInt) {
+        ProductInt = productInt;
+    }
+}
